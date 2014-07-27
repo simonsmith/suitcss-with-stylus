3e09312f15b170100a648cb1c3853d794d222297
Simon Smith <me@simonsmith.io>
2014-07-27 22:10:48 +0100

    Catch errors in watch task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var rework       = require('gulp-rework');
 var stylus       = require('gulp-stylus');
 var clean        = require('gulp-clean');
+var reworkSuit   = require('rework-suit');
 var runSequence  = require('run-sequence');
 
 var paths = {
@@ -12,9 +13,14 @@ var paths = {
   tempFolder: './built/'
 };
 
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
 gulp.task('compile-suit', function() {
   return gulp.src(paths.tempFolder + paths.mainCss)
-    .pipe(rework(require('rework-suit')()))
+    .pipe(rework(reworkSuit()).on('error', handleError))
     .pipe(gulp.dest('.'))
     .pipe(gulp.src(paths.tempFolder))
 });
